0522678beadb128088cbb04cdd4d8efef4427001
Soihiroudine <soihiroudine@outlook.fr>
2023-11-08 20:32:41 +0400

    Update
    - Réaménagement des position des éléments du dom
    - Ajout de fonctionnalité de mise en place de couleur aléatoire quand la page est rafraichit
    - Suppression de css superflu

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // const miniscule = "abcdefghijklmnopkrstuvwxyz";
 // const nombre = "0123456789";
 
-const couleur = Array("#90caf9", "#aed581", "#e57373", "#fff9c4");
+const couleur = Array("#90caf9", "#aed581", "#e57373", "#eae984");
 
 let corp = document.querySelector("body");
 
@@ -19,12 +19,20 @@ let buttonManipAfficheFormulaire = document.querySelector(".gros");
 let sectionCouleur = document.querySelector(".couleur");
 let buttonCouleur = document.querySelectorAll(".nth");
 
+// retourne une nombre aléatoire
+function getRandomInt(max) { return Math.floor(Math.random() * max);}
+
 function afficheFermeFormulaire(aff, sectAff, sectCoul, textButton){
     formulaire.style.display = aff; 
     sectionAffiche.style.top = sectAff;
     sectionCouleur.style.top = sectCoul;
     buttonManipAfficheFormulaire.innerHTML = textButton;
 }
+
+window.addEventListener("load", ()=>{
+    corp.style = "background-color:"+couleur[getRandomInt(couleur.length)]+";";
+    console.log("Bien.")
+})
 
 buttonManipAfficheFormulaire.addEventListener("click", () => {
     if(formulaire.style.display !== "none"){ afficheFermeFormulaire("none", "50%", "65%", "Ouvrir le formulaire"); }   
@@ -36,9 +44,17 @@ buttonMise.addEventListener("click", () =>{
     else{ afficher.innerHTML = mise.value; }
 });
 
+
+
 for(let i = 0; i < couleur.length; i++){
-    buttonCouleur[i].addEventListener("click", () =>{ corp.style.backgroundColor = couleur[i]; });
     buttonCouleur[i].style.backgroundColor = couleur[i];
+    buttonCouleur[i].addEventListener("mousedown", (e) =>{
+        e.target.style.backgroundColor = "#AAAAAA";
+        corp.style.backgroundColor = couleur[i];
+    });
+    buttonCouleur[i].addEventListener("mouseup", (e) =>{
+        e.target.style.backgroundColor = couleur[i];
+    });
 }
 
 copie.addEventListener("click", () =>{
